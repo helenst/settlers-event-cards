c0f8de99cc428fba7999906753c83d83916ba339
Helen ST <helenst@gmail.com>
2013-02-02 16:49:39 +0700

    Settings for sound effects and accelerometer

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from functools import partial
 
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.config import ConfigParser
 from kivy.core.audio import SoundLoader
+from kivy.uix.settings import Settings
 from kivy.uix.widget import Widget
 
 # Not used in here directly but import is needed for .kv file
@@ -16,11 +18,12 @@ from dice import DiceWidget
 
 class DiceScreen(Widget):
 
-    def __init__(self, **kwargs):
+    def __init__(self, play_sounds, **kwargs):
         super(DiceScreen, self).__init__(**kwargs)
         self.cards = []
         self.roll_sound = SoundLoader.load('diceroll.wav')
         self.rolling = False
+        self.play_sounds = play_sounds
 
     def renew_cards(self):
         self.cards = list(itertools.product(range(1, 7), range(1, 7)))
@@ -48,23 +51,45 @@ class DiceScreen(Widget):
             self.sum_label.text = '?'
             Clock.schedule_once(partial(animate_roll, 20), 0.05)
 
-            if self.roll_sound.state == 'play':
-                self.roll_sound.stop()
-            self.roll_sound.play()
+            if self.play_sounds:
+                if self.roll_sound.state == 'play':
+                    self.roll_sound.stop()
+                self.roll_sound.play()
 
 
 class DiceApp(App):
     def build(self):
-        screen = DiceScreen()
+        play_sounds = self.config.getint('sound', 'effects') == '1'
+        self.shake_to_roll = self.config.getint('accelerometer', 'shake') == '1'
+        self.screen = DiceScreen(play_sounds)
 
         import shake
         self.detector = shake.ShakeDetector()
 
         def on_shake(how_hard):
-            screen.roll_dice()
+            if self.shake_to_roll:
+                self.screen.roll_dice()
         self.detector.on_shake(on_shake)
 
-        return screen
+        return self.screen
+
+    def build_config(self, config):
+        config.setdefaults('sound', {
+            'effects': 1
+        })
+        config.setdefaults('accelerometer', {
+            'shake': 1
+        })
+
+    def build_settings(self, settings):
+        settings.add_json_panel('Dice', self.config, 'settings.json')
+
+    def on_config_change(self, config, section, key, value):
+        name = (section, key)
+        if name == ('sound', 'effects'):
+            self.screen.play_sounds = (value == '1')
+        elif name == ('accelerometer', 'shake'):
+            self.shake_to_roll = (value == '1')
 
 
 if __name__ in ('__main__', '__android__'):
